4bcf6ac2788658d7bf37365330cd2259233f63a7
NAVET Florence <fnavet@nge.fr>
2026-01-12 12:34:34 +0100

    Ajoutez des fichiers projet.

---
+++ JobOverview/Program.cs
@@ -0,0 +1,41 @@
+
+using JobOverview.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobOverview
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            string? connect = builder.Configuration.GetConnectionString("JobOverviewConnect");
+
+            // Add services to the container.
+            builder.Services.AddDbContext<ContexteJobOverview>(opt => opt.UseSqlServer(connect));
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
