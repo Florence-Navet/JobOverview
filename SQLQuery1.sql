bf25409530d80254c00f6ddfe0d89655942e29da
Florence Navet <enolaworld@gmail.com>
2026-02-07 16:06:19 +0100

    chore: modified sqlquery

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,1 +1,6 @@
-﻿DROP DATABASE IF EXISTS JobOverview;
+﻿SELECT Pseudo FROM Personnes;
+SELECT * FROM Logiciels WHERE Code = 'ANATOMIA';
+SELECT * FROM Modules WHERE Code = 'RADIO' AND CodeLogiciel = 'ANATOMIA';
+SELECT * FROM Versions WHERE Numero = 6 AND CodeLogiciel = 'ANATOMIA';
+
+SELECT * FROM Versions WHERE Numero = 6 AND CodeLogiciel = 'ANATOMIA';
