678945a4f9701b88d83c71a53d58f944cfaf9516
Florence Navet <enolaworld@gmail.com>
2026-03-01 19:27:11 +0100

    feat: add query

---
+++ SQLQuery3.sql
@@ -0,0 +1,7 @@
+﻿UPDATE AspNetUsers SET Id = 'APARENT', UserName = N'Agnès Parent' WHERE UserName = 'aparent@joboverview.fr';
+UPDATE AspNetUsers SET Id = 'BNORMAND', UserName = N'Bertrand Normand' WHERE UserName = 'bnormand@joboverview.fr';
+UPDATE AspNetUsers SET Id = 'JROUSSET', UserName = N'Joseph Rousset' WHERE UserName = 'jrousset@joboverview.fr';
+
+select * from AspnetUsers 
+SELECT Id, UserName, Email FROM AspNetUsers;
+
