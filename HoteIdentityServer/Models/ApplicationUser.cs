925d384c819fb0eb01a32986d52e65e3e9cd80e3
Florence Navet <enolaworld@gmail.com>
2026-03-01 11:53:51 +0100

    feat: configuration server

---
+++ HoteIdentityServer/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace HoteIdentityServer.Models
+{
+    public class ApplicationUser : IdentityUser<int>
+    {
+        public string Fonction { get; set; } = "";
+    }
+}
---
+++ HoteIdentityServer/Program.cs
@@ -0,0 +1,90 @@
+using Duende.IdentityServer.Models;
+using HoteIdentityServer.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddRazorPages();
+
+// Ajoute et configure le service IdentityServer
+builder.Services.AddIdentityServer(options =>
+      options.Authentication.CoordinateClientLifetimesWithUserSession = true)
+
+    // Cr�e des identit�s
+    .AddInMemoryIdentityResources(new IdentityResource[] {
+         new IdentityResources.OpenId(),
+         new IdentityResources.Profile(),
+    })
+
+     // Etendue d'API "entreprise" et revendications
+     .AddInMemoryApiScopes(new ApiScope[] {
+            new ApiScope("entreprise", new[] { "m�tier", "manager" })
+     })
+
+     // Configure une appli cliente
+     .AddInMemoryClients(new Client[] {
+         new Client
+         {
+            ClientId = "BlazorJO",
+            ClientSecrets = { new Secret("$xcuk%7yRLiKqD#r".Sha256()) },
+            AllowedGrantTypes = GrantTypes.Code,
+            
+            // Urls auxquelles envoyer les jetons
+            RedirectUris = { "https://localhost:7189/signin-oidc" },
+            // Urls de redirection apr�s d�connexion
+            PostLogoutRedirectUris = { "https://localhost:7189/signout-callback-oidc" },
+            // Url pour envoyer une demande de d�connexion au serveur d'identit�
+            FrontChannelLogoutUri = "https://localhost:7189/signout-oidc",
+
+				// Etendues d'API autoris�es
+				AllowedScopes = { "openid", "profile", "entreprise" },
+
+            // Autorise le client � utiliser un jeton d'actualisation
+				AllowOfflineAccess = true,
+            }
+    })
+    // Indique d'utiliser ASP.Net Core Identity pour la gestion des profils et revendications
+    .AddAspNetIdentity<IdentityUser>();
+
+// Ajoute la journalisation au niveau debug des �v�nements �mis par Duende
+builder.Services.AddLogging(options =>
+{
+    options.AddFilter("Duende", LogLevel.Debug);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+// Ajoute le middleware d'authentification avec IdentityServer dans le pipeline
+app.UseIdentityServer();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
